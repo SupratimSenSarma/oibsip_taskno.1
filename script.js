dff9201029f8b95dda088d98985915979701e3d8
SupratimSenSarma <127923822+SupratimSenSarma@users.noreply.github.com>
2023-03-29 10:34:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let scrn=document.getElementById('screen');
+
+function input(x){
+    scrn.value=scrn.value+x;
+}
+
+function allClear(){
+scrn.value=' ';
+}
+
+function del(){
+    scrn.value=scrn.value.slice(0,-1);
+}
+
+function calc(){
+    scrn.value=eval(scrn.value);
+}
